8bae73995fba14aec83f763ef73250c9eab7c7f2
maljackvroh <maljackdarius@gmail.com>
2024-06-30 16:13:51 +0800

    modified:   index.js 	modified:   node_modules/.package-lock.json 	new file:   node_modules/express-session/HISTORY.md 	new file:   node_modules/express-session/LICENSE 	new file:   node_modules/express-session/README.md 	new file:   node_modules/express-session/index.js 	new file:   node_modules/express-session/node_modules/cookie-signature/History.md 	new file:   node_modules/express-session/node_modules/cookie-signature/Readme.md 	new file:   node_modules/express-session/node_modules/cookie-signature/index.js 	new file:   node_modules/express-session/node_modules/cookie-signature/package.json 	new file:   node_modules/express-session/package.json 	new file:   node_modules/express-session/session/cookie.js 	new file:   node_modules/express-session/session/memory.js 	new file:   node_modules/express-session/session/session.js 	new file:   node_modules/express-session/session/store.js 	new file:   node_modules/on-headers/HISTORY.md 	new file:   node_modules/on-headers/LICENSE 	new file:   node_modules/on-headers/README.md 	new file:   node_modules/on-headers/index.js 	new file:   node_modules/on-headers/package.json 	new file:   node_modules/random-bytes/HISTORY.md 	new file:   node_modules/random-bytes/LICENSE 	new file:   node_modules/random-bytes/README.md 	new file:   node_modules/random-bytes/index.js 	new file:   node_modules/random-bytes/package.json 	new file:   node_modules/uid-safe/HISTORY.md 	new file:   node_modules/uid-safe/LICENSE 	new file:   node_modules/uid-safe/README.md 	new file:   node_modules/uid-safe/index.js 	new file:   node_modules/uid-safe/package.json 	modified:   package-lock.json 	modified:   package.json 	modified:   routes/auth_routes.js 	new file:   views/dashboard_admin.ejs 	new file:   views/dashboard_doctor.ejs 	new file:   views/home.ejs

--- index.js
+++ index.js
@@ -4,21 +4,35 @@ import cors from "cors";
 import UserRoute from "./routes/user_routes.js";
 import DoctorRoute from "./routes/doctor_routes.js";
 import AuthRoute from "./routes/auth_routes.js";
-
+import session  from "express-session";
 import { fileURLToPath } from "url";
+
 
 const app = express();
 const PORT = 3000;
+
+app.use(session({
+    secret:'this is my key',
+    saveUninitialized: false, 
+    resave: false,
+    cookie: {
+        secure: false,
+        maxAge: 30 * 60 * 1000
+    }
+}));
 
 // middleware untuk parsing  JSON dan URL-encoded from data
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+
+
 // mengatur direktori public 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: true}));
 
 // Mengatur view engine EJS
 app.set('view engine', 'ejs');
@@ -31,13 +45,12 @@ app.use(AuthRoute);
 
 
 app.get('/users/add', (req, res) => {
-    res.render('add_user'); // Render halaman 'add_user.ejs'
+    res.render('add_user'); 
 });
 
 app.get('/doctor/add', (req, res) => {
-    res.render('add_doctor'); // Render halaman 'add_user.ejs'
+    res.render('add_doctor');
 });
-
 
 app.get('/login_portal', (req, res) => {
     res.render('login_portal');
@@ -55,8 +68,16 @@ app.get('/news', (req, res) => {
     res.render('news');
 });
 
+app.get('/home', (req, res) => {
+    res.render('home');
+});
+
 app.get('/', (req, res) => {
-    res.render('homepage');
+    if (req.session.user){
+        res.render('home');
+    }else {
+        res.render('homepage');
+    }
 });
 
 app.listen(PORT, ()=> console.log(`server running on port ${PORT}...`));
--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bcrypt from 'bcrypt';
 import User from '../models/user_model.js';
-import { getUsers } from '../controllers/user_controller.js';
+// import { getUsers } from '../controllers/user_controller.js';
 
 const router = express.Router();
 
@@ -11,9 +11,8 @@ router.get('/login', (req, res) => {
 });
 
 // Route untuk memproses login
-router.post('/login', async (req, res) => {
+router.post('/login_user', async (req, res) => {
     const { username, password } = req.body;
-   
     try {
         const user = await User.findOne({ where: { username: username } });
 
@@ -25,12 +24,24 @@ router.post('/login', async (req, res) => {
         if (!match) {
             return res.status(401).json({ message: 'Incorrect password' });
         }
-
+        
+        req.session.user = user;
+        res.redirect('home');
         res.status(200).json({ message: 'Login successful', user });
     } catch (error) {
         console.error('Error logging in:', error);
         res.status(500).json({ message: 'Internal Server Error' });
     }
+});
+
+router.get('/logout', (req, res) => {
+    res.session.user.destroy(err => {
+        if (err) {
+            return res.redirect('/');
+        }
+        res.clearCookie('connect.sid');
+        res.redirect('/');
+    });
 });
 
 export default router;
