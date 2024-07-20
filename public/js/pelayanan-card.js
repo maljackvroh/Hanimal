66820caa72a64acec03f260dbc3dc7921e7dfd98
uswatulUlya03 <uswatululya03@gmail.com>
2024-07-20 13:39:01 +0800

    baruu

--- public/js/pelayanan-card.js
+++ public/js/pelayanan-card.js
@@ -1,37 +1,43 @@
 async function fetchDoctors() {
-    try {
-        const response = await fetch('http://localhost:3000/doctor');
-        const doctors = await response.json();
-        const container = document.getElementById('doctor-container');
-        doctors.forEach(doctor => {
-            const card = document.createElement('div');
-            card.className = 'card';
-            card.innerHTML = `
+  try {
+    const response = await fetch("http://hanimal.online:3000/doctor");
+    const doctors = await response.json();
+    const container = document.getElementById("doctor-container");
+    doctors.forEach((doctor) => {
+      const card = document.createElement("div");
+      card.className = "card";
+      card.innerHTML = `
                 <h2>${doctor.username}</h2>
-                ${doctor.spesialis ? `<p>${doctor.spesialis}</p>` : ''}
+                ${doctor.spesialis ? `<p>${doctor.spesialis}</p>` : ""}
                 <div class="buttons">
-                    <button class="profile-btn" onclick="redirectToProfile('${doctor.username}')">Profile</button>
-                    <button class="order-btn" onclick="redirectToOrder('${doctor.username}')">Pesan</button>
-                    <button class="discuss-btn" onclick="redirectToWhatsApp('${doctor.no_hp}')">Diskusi</button>
+                    <button class="profile-btn" onclick="redirectToProfile('${
+                      doctor.username
+                    }')">Profile</button>
+                    <button class="order-btn" onclick="redirectToOrder('${
+                      doctor.username
+                    }')">Pesan</button>
+                    <button class="discuss-btn" onclick="redirectToWhatsApp('${
+                      doctor.no_hp
+                    }')">Diskusi</button>
                 </div>
             `;
-            container.appendChild(card);
-        });
-    } catch (error) {
-        console.error('Error fetching doctors:', error);
-    }
+      container.appendChild(card);
+    });
+  } catch (error) {
+    console.error("Error fetching doctors:", error);
+  }
 }
 
 function redirectToProfile(username) {
-    window.location.href = `/profile?username=${username}`;
+  window.location.href = `/profile?username=${username}`;
 }
 
 function redirectToOrder(username) {
-    window.location.href = `/pesan?username=${username}`;
+  window.location.href = `/pesan?username=${username}`;
 }
 
 function redirectToWhatsApp(phone) {
-    window.location.href = `https://wa.me/${phone}`;
+  window.location.href = `https://wa.me/${phone}`;
 }
 
-document.addEventListener('DOMContentLoaded', fetchDoctors);
+document.addEventListener("DOMContentLoaded", fetchDoctors);
